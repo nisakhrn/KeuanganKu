08ecb585abbbac9fc04a416b9a05fa6cd593ac83
dian <diannazira@mhs.usk.ac.id>
2025-10-01 12:48:07 +0700

    add index

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function App() {
+  return (
+    <div>
+      <h1>Halo, KeuanganKu 🚀</h1>
+      <p>React sudah jalan!</p>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
